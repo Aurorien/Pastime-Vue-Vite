c3d61ce67612ac7f1494853b4a9f5d411e440781
Aurorien <starlightnoise@gmail.com>
2023-02-12 20:06:50 +0100

    Add Vue Vite code for project Pastime

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!-- components (ej router-componenter): det som syns på alla sidor -->
+<style lang="scss">
+  @import '../assets/main.scss';
+
+  main {
+    padding: $spacer;
+  }
+</style>
+
+<script>
+  import SiteHeader from './components/SiteHeader.vue'
+
+  export default {
+    components: {
+      SiteHeader
+    },
+    data() {
+      return { soundUrl: '' }
+    },
+    methods: {
+      onPunchlineFX() {
+        console.log('the punchlinefx-emit has landed')
+      }
+    }
+  }
+</script>
+
+<template>
+  <RouterLink to="/" />
+  <main>
+    <SiteHeader logo="Pastime" />
+    <RouterView @punchlinefx="onPunchlineFX" />
+  </main>
+</template>
---
+++ src/views/QuoteView.vue
@@ -0,0 +1,63 @@
+<style scoped lang="scss">
+  @import '../../assets/main.scss';
+</style>
+
+<script>
+  import axios from 'axios'
+  import SoundButton from '../components/SoundButton.vue'
+
+  export default {
+    async created() {
+      // doc: https://github.com/15Dkatz/official_joke_api
+      axios
+        .get('https://official-joke-api.appspot.com/jokes/programming/ten/')
+        .then((response) => {
+          console.log(response)
+          this.results = response.data
+          console.log('Resultat', this.results)
+        })
+    },
+    components: {
+      SoundButton
+    },
+    computed: {
+      name() {
+        return this.$store.state.name
+      }
+    },
+    data() {
+      return {
+        results: null,
+        listColor: 'lightgreen'
+      }
+    },
+    emits: ['punchlinefx'],
+    methods: {
+      togglePunchline(result) {
+        result.showPunchline = !result.showPunchline
+        this.$emit('punchlinefx')
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>Some {{ $route.params.theme }} jokes for you {{ name }}!</h1>
+  <li :key="result" v-for="result in results">
+    <div
+      :style="{
+        backgroundColor: result.showPunchline ? '#fae77c' : 'lightgreen'
+      }"
+    >
+      {{ result.setup }}
+      <!-- {{ result.showPunchline }} -->
+      <SoundButton @click="togglePunchline(result)">Show</SoundButton>
+      <span v-if="result.showPunchline" id="quote-span">
+        {{ result.punchline }}
+      </span>
+    </div>
+  </li>
+  <RouterLink class="d-flex flex-column align-items-center" to="/sound"
+    >To Sound</RouterLink
+  >
+</template>
